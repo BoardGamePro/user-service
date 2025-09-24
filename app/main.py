185299370ee72569f31d691a4157dc3f0ae84f82
Marchenko Dmitriy <dimamarch2014@gmail.com>
2025-09-24 14:45:09 +0500

    add cors

--- app/main.py
+++ app/main.py
@@ -4,6 +4,13 @@ from fastapi.security import HTTPBearer
 from .routers.auth import auth
 from .routers.users import users
 from .database import create_all
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,6 +25,13 @@ app = FastAPI(
     version="1.0.0",
     lifespan=lifespan,
     openapi_tags=[{"name": "auth", "description": "Authentication operations"}, {"name": "users", "description": "User operations"}]
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 app.include_router(auth)
 app.include_router(users)
